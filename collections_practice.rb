5909a017bc7f92611e7239c6fa41b9f8b65d7b5e
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-12-05 01:08:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,60 +4,75 @@ end
 
 def sort_array_desc(array)
   array.sort do |a,b|
-    b<=>a
+    b <=> a 
   end
 end
 
+# def sort_array_desc(array)
+#   array.sort.reverse
+# end
+
 def sort_array_char_count(array)
-  array.sort do |a,b|
-    a.length<=>b.length
-  end
+  array.sort_by! {|string| string.length }
 end
 
 def swap_elements(array)
-  array.sort do |a,b|
-    if array.index(a) == 0
-      0
-    elsif array.index(a) < array.index(b)
-      1
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |element| 
+    element[0].downcase == "a" 
+  end
+end
+
+def sum_array(array)
+  counter = 0 
+  array.each do |number|
+    counter += number 
+  end
+  counter
+end
+
+
+
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index != 1 
+      word << "s"
+    else 
+      word 
     end
   end
 end
 
-def swap_elements_from_to(array,id,destination_id)
-  selected_element = array.select{|element|array.index(element)==id}
-  after_destination = array.select{|element|array.index(element)>=destination_id && array.index(element)!= id}
-  before_destination = array.select{|element|array.index(element)<destination_id && array.index(element)!= id}
-  new_array = before_destination + selected_element + after_destination
-  return new_array
+def swap_elements_from_to(array, idx, new_idx)
+  array[idx], array[new_idx] = array[new_idx], array[idx]
+  array
+  
+  element = array.
+  
+  array_before_destination
 end
 
-def reverse_array(array)
-  array.sort do |a,b|
-    array.index(b) <=>  array.index(a)
-  end
-end
 
-def kesha_maker(array)
-  kesha_array = []
-  array.each{|word|
-    word[2]="$"
-    kesha_array << word
-  }
-  kesha_array
-end
 
-def find_a(array)
-  array.select { |word|word.start_with?("a")  }
-end
+# def sum_array(array)
+#   array.inject{|sum, num| sum + num }
+# end
 
-def sum_array(array)
-  array.reduce(:+)
-end
+# def sum_array(array)
+#   array.reduce(:+)
+# end
 
-def add_s(array)
-  array.each_with_index.collect{|word,id|
-   id == 1 ? word = word : word = word + "s"
-
-  }
-end
